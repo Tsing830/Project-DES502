cba1270bd3e881d054be9c8619f52f6fbda48f7c
CBigham07 <charlotte_bigham@icloud.com>
2026-03-24 14:40:27 +0000

    Added Warden Footsteps to WWise Project

--- Project-DES502_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Project-DES502_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMBIENCE1 = 1117071627U;
+        static const AkUniqueID PLAY_WARDENFOOTSTEPS = 956504242U;
     } // namespace EVENTS
 
     namespace BANKS
